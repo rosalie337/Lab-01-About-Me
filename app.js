9f285ebdbd2bcffeef6713dd00d12833e9614ac9
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-01-26 23:51:33 -0800

    Finally finished

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
-import checkNumbers from './guesser.js';
+import isYes from './is-yes.js';
 
-const userInput = document.getElementById('guess-input');
-const submitButton = document.getElementById('submit-button');
-const tempContainer = document.getElementById('temporary-number');
-const resultContainer = document.getElementById('result-container');
+const quizButton = document.getElementById('quiz-button');
+const totalScore = document.getElementById('results');
 
-const tempDisplay = document.getElementById('diplay-high');
-const resultDisplay = document.getElementById('win-display');
-const triesDisplay = document.getElementById('attempts');
-let tries = 4;
+quizButton.addEventListener('click', () => {
+    const nameFirst = prompt('Please tell us your name?');
+    const confirmation = confirm(`${nameFirst}, are you sure you are ready?`);
+    if (confirmation === false) {
+        return;
+    }
 
-let correctNumber = Math.floor(Math.random() * 10);
-submitButton.addEventListener('click', () => {
-    tries--;
-
-    triesDisplay.textContent = tries;
-    console.log('Dwight says', Number(userInput.value));
-    if (checkNumbers(Number(userInput.value), correctNumber) === -1) {
-        tempDisplay.textContent = 'too low';
-        tempContainer.classList.remove('hidden');
-    }
-    if (checkNumbers(Number(userInput.value), correctNumber) === 1) {
-        tempDisplay.textContent = 'too high';
-        tempContainer.classList.remove('hidden');
-    }
-    if (checkNumbers(Number(userInput.value), correctNumber) === 0) {
-        tempDisplay.textContent = 'correct';
-        tempContainer.classList.remove('hidden');
-        resultContainer.classList.remove('hidden');
-        sumbitButton.disabled = true;
-    }
-    if (tries === 0 && checkNumbers(Number(userInput.value), correctNumber) !== 0) {
-        submitButton.disabled = true;
-        resultContainer.classList.remove('hidden');
-        resultDisplay.textContent = 'lost';
-    }
+        /// three prompts to get text response from user
+    const answerOne = prompt('Has Rosalie lived in Oregon?');
+    const answerTwo = prompt('Was Rosalie born in the Philippines?');
+    const answerThree = prompt('Was Rosalie\'s s father in the Military?');
+    
+        // starting score at zero
+    let count = 0;
+    
+        //incrementing the score if the answer is Not yes or y
+    if (isYes(answerOne)) count += 1;
+    if (isYes(answerTwo)) count += 1;
+    if (isYes(answerThree)) count += 1;
+  
+    totalScore.textContent = `${nameFirst}, you got ${count} correct`;
 });

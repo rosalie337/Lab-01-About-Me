cf88985bb233c37e84c7bc6a7f626f073a9f6bdc
ROSALIE LEE <ROSALIE337@GMAIL.COM>
2020-02-02 20:54:29 -0800

    tries remaining works

--- Guessing Game/app.js
+++ Guessing Game/app.js
@@ -12,19 +12,20 @@ let tries = 4;
 
 let correctNumber = Math.floor(Math.random() * 10);
 submitButton.addEventListener('click', () => {
-    tries--;
+    tries = tries - 1;
+    // tries--;
 
     triesDisplay.textContent = tries;
     if (checkNumbers(Number(userInput.value), correctNumber) === -1) {
-        tempDisplay.textContent = 'too low';
+        tempDisplay.textContent = 'too low!';
         tempContainer.classList.remove('hidden');
     }
     if (checkNumbers(Number(userInput.value), correctNumber) === 1) {
-        tempDisplay.textContent = 'too high';
+        tempDisplay.textContent = 'too high!';
         tempContainer.classList.remove('hidden');
     }
     if (checkNumbers(Number(userInput.value), correctNumber) === 0) {
-        tempDisplay.textContent = 'correct';
+        tempDisplay.textContent = 'Correct!';
         tempContainer.classList.remove('hidden');
         resultContainer.classList.remove('hidden');
         submitButton.disabled = true;
@@ -32,6 +33,6 @@ submitButton.addEventListener('click', () => {
     if (tries === 0 && checkNumbers(Number(userInput.value), correctNumber) !== 0) {
         submitButton.disabled = true;
         resultContainer.classList.remove('hidden');
-        resultDisplay.textContent = 'lost';
+        resultDisplay.textContent = 'Inncorrect! Better luck next time.';
     }
 });
